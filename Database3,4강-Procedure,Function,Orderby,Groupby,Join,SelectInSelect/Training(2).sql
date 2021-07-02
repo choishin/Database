c0ff50608f3abddc2df636ef05ae860268a0d038
Lena Shin Choi <79796953+choishin@users.noreply.github.com>
2021-07-02 23:00:46 +0900

    Delete Database3,4강-Procedure,Function,Orderby,Groupby,Join,SelectInSelect directory

--- Database3,4강-Procedure,Function,Orderby,Groupby,Join,SelectInSelect/Training(2).sql
+++
@@ -1,4 +0,0 @@
-#show tables;
-#최단거리 찾기
-#SQRT(POWER(37.386052 - latitude,2) + POWER (127.1214038 - longtitude,2)) = (select MIN(SQRT(POWER(37.386052 - latitude,2) + POWER(127.1214038 - longtitude,2)))
-select number,place_addr_land,latitude,longtitude, SQRT(POWER(37.386052 - latitude,2) + POWER (127.1214038 - longtitude,2))as distance from freewifi;
